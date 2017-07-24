c60404026550ac3442b80fcf2a143cbc237d74a3
Markus Freise <mf@designbuero-freise.de>
2017-07-24 18:20:18 +0200

    Nur ein Test

--- DesignbueroFreisePlugIn.php
+++ DesignbueroFreisePlugIn.php
@@ -47,7 +47,7 @@ function my_login_logo() { ?>
         #login h1 a, .login h1 a {
             background-color: transparent;
             background-image: url(<?php echo plugins_url(); ?>/DesignbueroFreisePlugIn/Designbuero-Freise.png);
-            padding-bottom: 30px;
+            padding-bottom: 60px;
             width: 310px;
             height: 75px;
             background-size: contain;
